bd11a2eaef77767e73b01827eb7c8a4d04ea9854
meraeugene <andrew.villalon@1.ustp.edu.ph>
2026-03-15 12:57:46 +0800

    refactor: update section heights for consistency across AboutUsHero, ArticlesHero, ProjectDetailsHero, and ProjectsHero components

--- app/sections/(projects)/ProjectDetailsHero.tsx
+++ app/sections/(projects)/ProjectDetailsHero.tsx
@@ -19,7 +19,7 @@ const ProjectDetailsHero = ({ project }: { project: Project }) => {
   return (
     <section
       ref={containerRef}
-      className="relative h-screen overflow-hidden flex items-end pb-12 md:pb-24 px-4 md:px-12 2xl:px-24 font-inter"
+      className="relative h-[80vh] md:h-screen overflow-hidden flex items-end pb-12 md:pb-24 px-4 md:px-12 2xl:px-24 font-inter"
     >
       {/* Background image */}
       <div className="absolute inset-0 z-0">
